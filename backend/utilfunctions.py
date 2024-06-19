f3497a89e0edd34d5031b5e3dc4b64778b8b0712
jordon266 <159559967+jordon266@users.noreply.github.com>
2024-06-18 22:08:05 -0400

    Update utilfunctions.py
    removed comments

--- backend/utilfunctions.py
+++ backend/utilfunctions.py
@@ -4,7 +4,7 @@ import datetime
 # Make Generator Object into List
 def makelist(games):
     gameslist = []
-    for game in games:  #gAssuming the function is named export_by_player
+    for game in games:  
         print(game)
         gameslist.append(game)
     return gameslist
