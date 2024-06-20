f14c87521bb8903aaa5b2cfbf5aaa586b5162044
jordon266 <jordon.j.murray@torontomu.ca>
2024-06-19 21:23:45 -0400

    added get games based on speed

--- backend/server.py
+++ backend/server.py
@@ -13,16 +13,26 @@ client = berserk.Client(session=session)
 
 @app.route('/') # ‘https://www.google.com/‘
 def home():
-	return 'Hello, world!”'
-
-
-@app.route('/mygames')
-def getmygames():
     try:
         games = utilfunctions.makelist(client.games.export_by_player("titledKing23"))
-        return games
+        utilfunctions.prettyprint(games)
+        return "games accessed"
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error":str(e)})
+    return  "Test"
+
+@app.route('/myblitzgames')
+def myblitzgames():
+    try:    
+        return utilfunctions.getgames('blitz')
     except requests.exceptions.RequestException as e:
         return jsonify({"error":str(e)})
 
+@app.route('/myradidgames')
+def myrapidgames():
+    try:    
+        return utilfunctions.getgames('rapid')
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error":str(e)})
 
 app.run(port=5000)
