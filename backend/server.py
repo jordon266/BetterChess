3b7578a869e24d93f86d42a95f3492061724bd3f
jordon266 <jordon.j.murray@torontomu.ca>
2024-06-22 00:13:17 -0400

    updated and removed json file

--- backend/server.py
+++ backend/server.py
@@ -8,30 +8,49 @@ app = Flask(__name__)
 apikey = os.environ['lichesskey']
 session = berserk.TokenSession(apikey)
 client = berserk.Client(session=session)
-
+data = utilfunctions.utils()
 
 
 @app.route('/') # ‘https://www.google.com/‘
 def home():
     try:
-        games = utilfunctions.makelist(client.games.export_by_player("titledKing23"))
-        utilfunctions.prettyprint(games)
-        return "games accessed"
+        return data.getallgames()
     except requests.exceptions.RequestException as e:
         return jsonify({"error":str(e)})
     return  "Test"
 
 @app.route('/myblitzgames')
 def myblitzgames():
-    try:    
-        return utilfunctions.getgames('blitz')
+    try:   
+        return data.getgamesbyspeed('blitz')
     except requests.exceptions.RequestException as e:
         return jsonify({"error":str(e)})
 
-@app.route('/myradidgames')
+@app.route('/myrapidgames')
 def myrapidgames():
     try:    
-        return utilfunctions.getgames('rapid')
+        return data.getgamesbyspeed('rapid')
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error":str(e)})
+
+@app.route('/mywhitegames')
+def mywhitegames():
+    try:    
+        return data.getgamesbycolor('white','titledKing23')
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error":str(e)})
+    
+@app.route('/myblackgames')
+def myblackgames():
+    try:    
+        return data.getgamesbycolor('black' ,'titledKing23')
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error":str(e)})
+
+@app.route('/allwins')
+def mywins():
+    try:    
+        return data.getgamesbycolor('black' ,'titledKing23')
     except requests.exceptions.RequestException as e:
         return jsonify({"error":str(e)})
 
--- backend/utilfunctions.py
+++ backend/utilfunctions.py
@@ -2,38 +2,50 @@
 import datetime
 from flask import Flask, jsonify
 import json
+import os
+import berserk
 
-# load json file into variable
-def loadfile(fname):
-    with open(fname,'r') as f:
-        data = json.load(f)     
-    f.close()
-    return data 
 
-# Make Generator Object into List
-def makelist(games):
-    gameslist = []
-    for game in games:  
-        # print(game)
-        gameslist.append(game)
-    return gameslist
 
-# Takes a List turns it into a string and organizes it so that it can be used in a json fileclea
-def prettyprint(obj):
-    with open('games.json', 'w') as f:
-        f.write(jsonify(obj).data.decode('utf-8'))
-    f.close()
+class utils:
+    def __init__(self):
+        # empty list
+        self.mygames = []
+        # name of app
+        self.app = Flask(__name__)
+        # apikey
+        self.apikey = os.environ['lichesskey']
+        #use api key to set session
+        self.session = berserk.TokenSession(self.apikey)
+        # use session to create a client
+        self.client = berserk.Client(session=self.session)
+        # get generator object returned by export players
+        self.genobj = self.client.games.export_by_player("titledKing23")
+        # turn genobj into list
+        for game in self.genobj:
+            self.mygames.append(game)
+            
+     # gets all games       
+    def getallgames(self):    
+        return self.mygames
+    
+    # get games by speed 
+    def getgamesbyspeed(self, speed): 
+        gamesbyspeed = []
+        for val in self.mygames:
+            if val['speed'] == speed:
+               gamesbyspeed.append(val)
+        return gamesbyspeed
+    # get games by color and username
+  
+    def getgamesbycolor(self, color,name):
+        games = []
+        for game in self.mygames:
+              # some game objects dont hav user in their so have to account for that
+            if 'user' in game['players'][color]:
+                if game['players'][color]['user']['name'] == name:
+                    print(game['players'][color]['user']['name'] )
+                    games.append(game)
+                print(game)   
+        return games
 
-# Gets All Games from json file
-def getgames():
-    data = loadfile('games.json')     
-    return data
-
-# Gets All Games from json file based on speed
-def getgames(speed):
-    blitzgames = []
-    data = loadfile('games.json')     
-    for val in data:
-        if val['speed'] == speed:
-            blitzgames.append(val)
-    return blitzgames
