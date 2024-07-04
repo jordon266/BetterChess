b9ff8efa954a51a5f5114cab0a843536b465e939
jordon266 <jordon.j.murray@torontomu.ca>
2024-07-04 01:06:17 -0400

    refactor test for request-analysis

--- backend/requesthandler.py
+++ backend/requesthandler.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import time
 # get all my games
 
     # get games put into list
@@ -10,25 +10,33 @@ token ='lip_88V9PY5fawgJg6mBIVeM'
 uname ='titledKing23'
 password ='rT+Chv9BLpz)GRP'
 gameslist = []
+params = {'since':'true','until':'true', 'color':'true', 'analysed':'true', 'vs':'true','rated':'true','moves':'true','literate':'true','sort':'true'}
+default_api_endpoint_params = 'opening=true&evals=true&clocks=true'
 
-api_endpoint_params = 'opening=true&evals=true&clocks=true'
 def api_getallgamesbyuser(username,token):  
     url = f'https://lichess.org/api/games/user/{username}?'     
     headers = {'Accept': 'application/x-ndjson','Authorization': f'Bearer {token}'}
-    response = requests.request("GET", url+api_endpoint_params, headers=headers)
+    response = requests.request("GET", url+default_api_endpoint_params, headers=headers)
     games= [json.loads(line, object_hook=dict) for line in response.text.splitlines()]
     return games
             
-def request_analysis(username,password,gameid):
+def request_analysis(username,password):
     url = "https://lichess.org:443/login"
-    request_analysis_url = f'https://lichess.org/{gameid}/request-analysis'
+    
     headers = {"Cache-Control": "max-age=0", "Sec-Ch-Ua": "\"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"", "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundary1mNByeI9bEiVtpDG", "X-Requested-With": "XMLHttpRequest", "Sec-Ch-Ua-Mobile": "?0", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.112 Safari/537.36", "Sec-Ch-Ua-Platform": "\"Linux\"", "Accept": "*/*", "Origin": "https://lichess.org", "Sec-Fetch-Site": "same-origin", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Dest": "empty", "Referer": "https://lichess.org/login", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8", "Priority": "u=1, i"}
     data = f"------WebKitFormBoundary1mNByeI9bEiVtpDG\r\nContent-Disposition: form-data; name=\"username\"\r\n\r\n{username}\r\n------WebKitFormBoundary1mNByeI9bEiVtpDG\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\n{password}\r\n------WebKitFormBoundary1mNByeI9bEiVtpDG\r\nContent-Disposition: form-data; name=\"token\"\r\n\r\n\r\n------WebKitFormBoundary1mNByeI9bEiVtpDG--\r\n"
     # get lila2 cookie for request-analysis
     session = requests.session() 
-    session.post(url, headers=headers, data=data)
-    session.post(request_analysis_url, headers=headers)
-
+    r =  session.post(url, headers=headers, data=data)
+    print(r)
+    ids = getidfromgames(username=uname,token=token) 
+    for id in ids:     
+        request_analysis_url = f'https://lichess.org/{id}/request-analysis'
+        resp = session.post(request_analysis_url, headers=headers)
+        print(resp)
+        time.sleep(35)
+    print('done')
+    
  
 def getidfromgames(username,token):
     results =[]
@@ -37,6 +45,9 @@ def getidfromgames(username,token):
         if 'analysis' not in game:
             results.append(game['id'])
     return results
+
+
+
 # Function below for debugging purposes 
 def getallgamesinfo(username,token):
     gamesinfo = { 'totgames': 0, 'toteval': 0, 'totwhite': 0, 'totblack': 0,'blitznum' : 0, 'rapidnum': 0,  'classinum' : 0,'vscompnum' : 0,'vswhiteai' : 0, 'totwins': 0, 'totloss' : 0} 
@@ -98,6 +109,10 @@ def getallgamesinfo(username,token):
 ids = getidfromgames(username=uname,token=token) 
 print(ids)
 gameinfo = getallgamesinfo(username=uname,token=token)
+print(gameinfo)  
+request_analysis(uname,password=password)
+gameinfo = getallgamesinfo(username=uname,token=token)
+
 print(gameinfo)  
 
             
--- backend/server.py
+++ backend/server.py
@@ -6,47 +6,47 @@ import  utilfunctions
 data = utilfunctions.utils()
 
 
-@app.route('/') # ‘https://www.google.com/‘
+@app.route('/') 
 def home():
-    try:
-        return data.getallgames()
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error":str(e)})
+    # try:
+    #     return data.getallgames()
+    # except requests.exceptions.RequestException as e:
+    #     return jsonify({"error":str(e)})
     return  "Test"
 
 @app.route('/myblitzgames')
 def myblitzgames():
-    try:   
-        return data.getgamesbyspeed('blitz')
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error":str(e)})
+    # try:   
+    #     return data.getgamesbyspeed('blitz')
+    # except requests.exceptions.RequestException as e:
+    return jsonify({"error":str(e)})
 
 @app.route('/myrapidgames')
 def myrapidgames():
-    try:    
-        return data.getgamesbyspeed('rapid')
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error":str(e)})
+    # try:    
+    #     return data.getgamesbyspeed('rapid')
+    # except requests.exceptions.RequestException as e:
+    return jsonify({"error":str(e)})
 
 @app.route('/mywhitegames')
 def mywhitegames():
-    try:    
-        return data.getgamesbycolor('white','titledKing23')
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error":str(e)})
+    # try:    
+    #     return data.getgamesbycolor('white','titledKing23')
+    # except requests.exceptions.RequestException as e:
+    return jsonify({"error":str(e)})
     
 @app.route('/myblackgames')
 def myblackgames():
-    try:    
-        return data.getgamesbycolor('black' ,'titledKing23')
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error":str(e)})
+    # try:    
+    #     return data.getgamesbycolor('black' ,'titledKing23')
+    # except requests.exceptions.RequestException as e:
+    return jsonify({"error":str(e)})
 
 @app.route('/allwins')
 def mywins():
-    try:    
-        return data.getgamesbycolor('black' ,'titledKing23')
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error":str(e)})
+    # try:    
+    #     return data.getgamesbycolor('black' ,'titledKing23')
+    # except requests.exceptions.RequestException as e:
+    return jsonify({"error":str(e)})
 
 app.run(port=5000)
