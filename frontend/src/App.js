eeca506f426e954ba89546315559fc007d63adbc
jordon266 <jordon.j.murray@torontomu.ca>
2024-07-25 23:58:22 -0400

    fixed fetch being called twice on front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,57 +5,56 @@ import { useEffect, useState } from 'react'
 
 function App() {
   // got from chess.com
-  const [numOfGames, setnumOfGames] = useState(0);
- let games = []
- async function test(response) {
-  const reader = response.body.getReader();
-  const decoder = new TextDecoder("utf-8");
-  const lineMatcher = /\n/;
-  let chunkCount = 0;
-  let games = [];
-  new ReadableStream().getReader().read()
-  try {
-    while (true) {
-      const { done, value } = await reader.read();
-      if (done) break;
-      const chunk = decoder.decode(value, { stream: true });
-      // chunkCount++;
-      let parts = chunk.split(lineMatcher)
-      if (chunk.includes('}{')){
-         let chunk_1 = chunk.replaceAll(/}{/g,"},,{")
-         parts = chunk_1.split(/,,/)
-        console.log('number of parts:' + parts.length)
-      }
-      for (let part of parts){
-        try {
-         games.push(JSON.parse(part))
-         console.log(games.length)
-        } catch (error) {
-          console.log(error)
+  const [games, setGame] = useState([]);
+
+
+  async function test(response) {
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder("utf-8");
+    const lineMatcher = /\n/;
+    let num = 0;
+    try {
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        const chunk = decoder.decode(value, { stream: true });
+        // chunkCount++;
+        let parts = chunk.split(lineMatcher)
+        if (chunk.includes('}{')) {
+          let chunk_1 = chunk.replaceAll(/}{/g, "},,{")
+          parts = chunk_1.split(/,,/)
+          console.log('number of parts:' + parts.length)
         }
-      }  
+        for (let part of parts) {
+        
+            let game =JSON.parse(part)
+            setGame(oldArray=>[...oldArray,game])
+        }
+
+      }
+
+    } catch (error) {
+      console.error('Stream processing error:', error);
     }
-  } catch (error) {
-    console.error('Stream processing error:', error);
-  }
-  return games;
-}
-
-
   
+    return games;
+  }
+
+
+
   useEffect(() => {
     const apiUrl = '/mygames';
-    fetch(apiUrl)
-.then(response => test(response)).then(console.log(games)) // console.log not working gotta see out fetch and then work
-  .catch(error => console.error(error));
-});
+    fetch(apiUrl) // being called twice --> solved
+      .then(response => test(response)).then(games => console.log(games)) // console.log not working gotta see out fetch and then work
+      .catch(error => console.error('error'));
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js {games.length}</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -65,7 +64,13 @@ function App() {
         >
           Learn React
         </a>
-        Number of Games
+
+        <ol>
+        {games.map(game=> {
+          return <li>{game['id']}</li>;
+        })}
+        
+        </ol>
       </header>
     </div>
   );
